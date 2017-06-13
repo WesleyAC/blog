99ae88f77e5c39824df6791ead9db54e82d812a5
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-06-13 10:43:42 -0700

    Remove scrollreveal effect

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -5,16 +5,3 @@
 $(function () {
   $('.fluidbox-trigger').fluidbox();
 })
-
-// Initialize scrollreveal
-
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('.reveal', {
-  distance: '0',
-  duration: 500,
-  easing: 'ease-in-out',
-  origin: 'top',
-  scale: 1,
-  reset: false,
-  viewFactor: 0
-});
