34d00e6a595cdf088ffb6f93ae72e6375e531950
Wesley Aptekar-Cassels <me@wesleyac.com>
2022-11-13 00:22:41 -0500

    Fix build problems.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-with import <nixpkgs> {};
+with import (builtins.fetchTarball {
+  name = "nixos-pinned-for-blog";
+  url = "https://github.com/nixos/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz";
+  #sha256 = "0n2xxk9pzwlbia80xxw03zcxd1qac89vn4mphx5l63r7vd71m54k";
+  sha256 = "sha256:162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+}) {};
 
 stdenv.mkDerivation {
   name = "wesleyac-blog";
@@ -7,5 +12,7 @@ stdenv.mkDerivation {
     minify
     bundler
     libxml2
+    pkgconfig
+    libxslt
   ];
 }
